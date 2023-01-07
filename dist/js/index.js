36d92c7e09ef24517b97437c2cd41d8cf74990cc
liviafort <livia.fortunato@academico.ifpb.edu.br>
2023-01-07 10:05:50 -0300

    Compilação - tipos

---
+++ dist/js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+//como declarar variavel?
+const n = 2;
+const x = 'ola';
+let y = [];
+let isNumber = false;
+let numeros = [1, 2, 3, 4, 5];
+//verificar tipo
+console.log(typeof isNumber);
+console.log(typeof x);
+//verificar tamanho do array
+console.log(numeros.length);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+//como declarar variavel?
+const n: number = 2;
+const x: string = 'ola';
+let y: string[] = [];
+let isNumber: boolean = false;
+let numeros: number[] = [1, 2, 3, 4, 5]
+//verificar tipo
+console.log(typeof isNumber);
+console.log(typeof x);
+//verificar tamanho do array
+console.log(numeros.length);
